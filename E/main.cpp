3a56f967e88971d7f6147dbc3e5f9237af44ef60
KhuzinT <KhuzinTka@yandex.ru>
2021-12-12 19:23:52 +0300

    task E v0.1

--- E/main.cpp
+++ E/main.cpp
@@ -4,129 +4,157 @@
 #include <vector>
 #include <stack>
 #include <memory>
-
-//********************************************************************************************
-
-using Vertex = int32_t;
-
-using WeightT = int32_t;
-
-const int32_t kInfinity = 1000 * 1000 * 1000 * 1LL;
-
 //********************************************************************************************
 
 class IGraph {
+public:
+    using Vertex = int32_t;
+
+    using WeightT = int32_t;
+
+    inline static const int32_t kINFINITY = 1000 * 1000 * 1000 * 1LL;
+
 protected:
     int32_t q_vertex_ = 0;
 
-    virtual void Add(const Vertex& a, const Vertex& b, const Vertex& w) = 0;
+    virtual void Add(const Vertex& begin, const Vertex& end, const WeightT& weight) = 0;
 
 public:
     [[nodiscard]] int32_t GetQVertex() const {
         return q_vertex_;
     }
 
-    void AddEdge(const Vertex& begin, const Vertex& end, const WeightT& weight) {
+    void AddWeightEdgeInNumberingFromOne(const Vertex& begin, const Vertex& end, const WeightT& weight) {
         Add(begin - 1, end - 1, weight);
     }
 
-    virtual std::vector<Vertex> GetNeighbors(const Vertex& v) const = 0;
+    void AddWeightEdgeInNumberingFromZero(const Vertex& begin, const Vertex& end, const WeightT& weight) {
+        Add(begin, end, weight);
+    }
 
-    virtual Vertex GetVertex(const Vertex& begin, const Vertex& end) const = 0;
+    void AddEdgeInNumberingFromOne(const Vertex& begin, const Vertex& end) {
+        Add(begin - 1, end - 1, 0);
+    }
 
-    virtual void ChangeVertex(const Vertex& begin, const Vertex& end, const int32_t& diff) = 0;
+    void AddEdgeInNumberingFromZero(const Vertex& begin, const Vertex& end) {
+        Add(begin, end, 0);
+    }
+
+    virtual std::vector<Vertex> GetNeighbors(const Vertex& current_vertex) const = 0;
+
+    virtual Vertex GetFlowOfEdgeFromBeginToEnd(const Vertex& begin, const Vertex& end) const = 0;
+
+    virtual void ChangeFlowOfEdgeFromBeginToEndByTheAmountOfDiff(const Vertex& begin, const Vertex& end,
+                                                                 const WeightT& diff) = 0;
 };
 
 class GraphMatrix final : public IGraph {
 private:
     std::vector<std::vector<Vertex>> matrix_;
 
-    void Add(const Vertex& a, const Vertex& b, const WeightT& w) override {
-        matrix_[a][b] = w;
+    void Add(const Vertex& begin, const Vertex& end, const WeightT& weight) override {
+        matrix_[begin][end] = weight;
     }
 
 public:
-    explicit GraphMatrix(const Vertex& quantity) {
-        std::vector<Vertex> tmp(quantity, 0);
-        matrix_.resize(quantity, tmp);
+    explicit GraphMatrix(const int32_t& quantity) {
+        matrix_.resize(quantity, std::vector<Vertex>(quantity, 0));
         q_vertex_ = quantity;
     }
 
-    [[nodiscard]] std::vector<Vertex> GetNeighbors(const Vertex& v) const override {
-        return matrix_[v];
+    explicit GraphMatrix(const IGraph& other_graph) : IGraph(other_graph) {
+        matrix_ = dynamic_cast<const GraphMatrix*>(&other_graph)->matrix_;
     }
 
-    [[nodiscard]] Vertex GetVertex(const Vertex& begin, const Vertex& end) const override {
+    [[nodiscard]] std::vector<Vertex> GetNeighbors(const Vertex& current_vertex) const override {
+        std::vector<Vertex> neighbors;
+        for (int32_t iteration = 0; iteration < q_vertex_; ++iteration) {
+            if (matrix_[current_vertex][iteration] > 0) {
+                neighbors.push_back(iteration);
+            }
+        }
+        return neighbors;
+    }
+
+    [[nodiscard]] Vertex GetFlowOfEdgeFromBeginToEnd(const Vertex& begin, const Vertex& end) const override {
         return matrix_[begin][end];
     }
 
-    void ChangeVertex(const Vertex& begin, const Vertex& end, const int32_t& diff) override {
+    void ChangeFlowOfEdgeFromBeginToEndByTheAmountOfDiff(const Vertex& begin, const Vertex& end,
+                                                         const int32_t& diff) override {
         matrix_[begin][end] += diff;
     }
 };
 
 //********************************************************************************************
 
-void DFS(IGraph& graph, const Vertex& start, std::vector<bool>& visit, std::vector<Vertex>& parent) {
-    std::stack<Vertex> stack;
+void DfsToFindFlowInGraph(IGraph& graph, const IGraph::Vertex& start, std::vector<bool>& visit,
+                          std::vector<IGraph::Vertex>& parent) {
+    std::stack<IGraph::Vertex> stack;
     stack.push(start);
     visit[start] = true;
 
     while (!stack.empty()) {
-        Vertex v = stack.top();
+        IGraph::Vertex current_vertex = stack.top();
         stack.pop();
-        auto neighbors = graph.GetNeighbors(v);
-        for (Vertex u = 0; u < graph.GetQVertex(); ++u) {
-            if (neighbors[u] > 0 && u != parent[v]) {
-                if (!visit[u]) {
-                    stack.push(u);
-                    parent[u] = v;
-                    visit[u] = true;
+        auto neighbors = graph.GetNeighbors(current_vertex);
+        for (auto& new_vertex : neighbors) {
+            if (new_vertex != parent[current_vertex]) {
+                if (!visit[new_vertex]) {
+                    stack.push(new_vertex);
+                    parent[new_vertex] = current_vertex;
+                    visit[new_vertex] = true;
                 }
             }
         }
     }
 }
 
-int32_t FordFulkersonAlgorithm(IGraph& graph) {
-    Vertex start = 0;
-    Vertex finish = graph.GetQVertex() - 1;
+IGraph::WeightT GetCurrentFlow(const IGraph& graph, const IGraph::Vertex& start, const IGraph::Vertex& finish,
+                               const std::vector<IGraph::Vertex>& parent) {
+    IGraph::WeightT flow = IGraph::kINFINITY;
 
-    int32_t answer = 0;
+    for (IGraph::Vertex current = finish; current != start; current = parent[current]) {
+        IGraph::Vertex prev = parent[current];
+        flow = std::min(flow, graph.GetFlowOfEdgeFromBeginToEnd(prev, current));
+    }
 
-    std::vector<bool> visit(graph.GetQVertex(), false);
-    std::vector<Vertex> parent(graph.GetQVertex(), kInfinity);
+    return flow;
+}
+
+void ChangeCurrentFlowInGraph(IGraph& graph, const IGraph::Vertex& start, const IGraph::Vertex& finish,
+                              const std::vector<IGraph::Vertex>& parent, const IGraph::WeightT& flow) {
+    for (IGraph::Vertex current = finish; current != start; current = parent[current]) {
+        IGraph::Vertex prev = parent[current];
+
+        graph.ChangeFlowOfEdgeFromBeginToEndByTheAmountOfDiff(prev, current, -flow);
+        graph.ChangeFlowOfEdgeFromBeginToEndByTheAmountOfDiff(current, prev, flow);
+    }
+}
+
+int32_t FordFulkersonAlgorithm(const IGraph& graph, const IGraph::Vertex& start, const IGraph::Vertex& finish) {
+    GraphMatrix graph_copy(graph);
+    int32_t max_flow = 0;
+
+    std::vector<bool> visit(graph_copy.GetQVertex(), false);
+    std::vector<IGraph::Vertex> parent(graph_copy.GetQVertex(), IGraph::kINFINITY);
 
     while (true) {
-        DFS(graph, start, visit, parent);
+        DfsToFindFlowInGraph(graph_copy, start, visit, parent);
         if (!visit[finish]) {
             break;
         }
 
-        WeightT flow = kInfinity;
+        auto flow = GetCurrentFlow(graph_copy, start, finish, parent);
+        ChangeCurrentFlowInGraph(graph_copy, start, finish, parent, flow);
 
-        for (Vertex current = finish; current != start; current = parent[current]) {
-            Vertex prev = parent[current];
-            flow = std::min(flow, graph.GetVertex(prev, current));
-        }
+        max_flow += flow;
 
-        for (Vertex current = finish; current != start; current = parent[current]) {
-            Vertex prev = parent[current];
-
-            graph.ChangeVertex(prev, current, -flow);
-            graph.ChangeVertex(current, prev, flow);
-        }
-
-        answer += flow;
-
-        parent.clear();
-        visit.clear();
-
-        parent.resize(graph.GetQVertex(), kInfinity);
-        visit.resize(graph.GetQVertex(), false);
+        parent.assign(graph_copy.GetQVertex(), IGraph::kINFINITY);
+        visit.assign(graph_copy.GetQVertex(), false);
     }
 
-    return answer;
+    return max_flow;
 }
 
 int main() {
@@ -139,15 +167,15 @@ int main() {
 
     GraphMatrix graph(q_vertex);
 
-    for (int32_t i = 0; i < q_edge; ++i) {
-        Vertex begin = 0;
-        Vertex end = 0;
-        WeightT weight = 0;
+    for (int32_t iteration = 0; iteration < q_edge; ++iteration) {
+        IGraph::Vertex begin = 0;
+        IGraph::Vertex end = 0;
+        IGraph::WeightT weight = 0;
         std::cin >> begin >> end >> weight;
-        graph.AddEdge(begin, end, weight);
+        graph.AddWeightEdgeInNumberingFromOne(begin, end, weight);
     }
 
-    auto answer = FordFulkersonAlgorithm(graph);
+    auto answer = FordFulkersonAlgorithm(graph, 0, graph.GetQVertex() - 1);
     std::cout << answer << std::endl;
 
     return 0;
